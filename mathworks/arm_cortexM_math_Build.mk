e985c86155e8d400e461f43cc2c51bd745f86d2b
mw-rkandur <40785458+mw-rkandur@users.noreply.github.com>
2018-07-13 12:57:53 +0530

    Fixes for Windows

--- mathworks/arm_cortexM_math_Build.mk
+++ mathworks/arm_cortexM_math_Build.mk
@@ -2,31 +2,80 @@
 ifeq ($(OS),Windows_NT)
 MAKEDIR := mkdir
 MAKEDIRFLAGS := 
+RM := del
+RMFLAGS := /q /f
 CP := xcopy
 CPFLAGS := /e /f /r /y /q
 CPWILDCARD := 
 else
 MAKEDIR := mkdir
 MAKEDIRFLAGS := -p
+RM := rm
+RMFLAGS := -rf
 CP := cp
 CPFLAGS := -r
-CPWILDCARD := *
+CPWILDCARD := /*
+endif
+
+
+# CMSIS Packaging directory list
+CMSISPKG := ./CMSIS
+CMSIS := ../CMSIS
+CMSISPKGINCLUDE := $(CMSISPKG)/Include
+CMSISPKGLIB := $(CMSISPKG)/Lib
+CMSISPKGRTOS := $(CMSISPKG)/RTOS
+CMSISLIB := $(CMSIS)/Lib
+CMSISMWLIB := ./Lib$(CPWILDCARD)
+CMSISINCLUDE := $(CMSIS)/Include
+CMSISRTOS := $(CMSIS)/RTOS
+CMSISEULAPDF := $(CMSIS)/CMSIS_END_USER_LICENCE_AGREEMENT.pdf
+CMSISEULARTF := $(CMSIS)/CMSIS_END_USER_LICENCE_AGREEMENT.rtf
+CMSISPKGEULAPDF := $(CMSISPKG)/CMSIS_END_USER_LICENCE_AGREEMENT.pdf
+CMSISPKGEULARTF := $(CMSISPKG)/CMSIS_END_USER_LICENCE_AGREEMENT.rtf
+
+ifeq ($(OS),Windows_NT)
+CMSISPKG := $(subst /,\,$(CMSISPKG))
+CMSISPKGINCLUDE := $(subst /,\,$(CMSISPKGINCLUDE))
+CMSISPKGLIB := $(subst /,\,$(CMSISPKGLIB))
+CMSISPKGRTOS := $(subst /,\,$(CMSISPKGRTOS))
+CMSISLIB := $(subst /,\,$(CMSISLIB))
+CMSISMWLIB := $(subst /,\,$(CMSISMWLIB))
+CMSISINCLUDE := $(subst /,\,$(CMSISINCLUDE))
+CMSISRTOS := $(subst /,\,$(CMSISRTOS))
+CMSISEULAPDF := $(subst /,\,$(CMSISEULAPDF))
+CMSISEULARTF := $(subst /,\,$(CMSISEULARTF))
+CMSISPKGEULAPDF := $(subst /,\,$(CMSISPKGEULAPDF))
+CMSISPKGEULARTF := $(subst /,\,$(CMSISPKGEULARTF))
 endif
 
 
 .PHONY : all build clean info
 
-all : build
+$(CMSISPKGEULAPDF) : 
+	$(CP) $(CMSISEULAPDF) $(CMSISPKGEULAPDF)
+
+$(CMSISPKGEULARTF) : 
+	$(CP) $(CMSISEULARTF) $(CMSISPKGEULARTF)
+
+$(CMSISPKG) : 
 	@echo Creating CMSIS package
-	$(MAKEDIR) $(MAKEDIRFLAGS) ./CMSIS
-	@echo	Copying DSP library from actual CMSIS
-	$(CP) $(CPFLAGS) ../CMSIS/Lib ./CMSIS/Lib
-	@echo	Copying DSP library from actual Mathworks CMSIS
-	$(CP) $(CPFLAGS) ./Lib/$(CPWILDCARD) ./CMSIS/Lib
-	@echo	Copying Include directory
-	$(CP) $(CPFLAGS) ../CMSIS/Include ./CMSIS/Include
+	$(MAKEDIR) $(MAKEDIRFLAGS) $(CMSISPKG)
+
+$(CMSISPKGINCLUDE) : 
+	@ echo	Copying Include directory
+	$(CP) $(CPFLAGS) $(CMSISINCLUDE) $(CMSISPKGINCLUDE)
+
+$(CMSISPKGLIB) : 
+	@echo	Copying DSP library from CMSIS package
+	$(CP) $(CPFLAGS) $(CMSISLIB) $(CMSISPKGLIB)
+	@echo	Copying DSP library compiled with Soft ABI FPU
+	$(CP) $(CPFLAGS) $(CMSISMWLIB) $(CMSISPKGLIB)
+
+$(CMSISPKGRTOS) :
 	@echo	Copying RTOS
-	$(CP) $(CPFLAGS) ../CMSIS/RTOS ./CMSIS/RTOS
+	$(CP) $(CPFLAGS) $(CMSISRTOS) $(CMSISPKGRTOS)
+	
+all : build $(CMSISPKG) $(CMSISPKGEULAPDF) $(CMSISPKGEULARTF) $(CMSISPKGINCLUDE) $(CMSISPKGLIB) $(CMSISPKGRTOS)
 
 build :
 	@echo   ----------------------------------------------------------------------------
@@ -61,7 +110,7 @@ clean :
 	make -s -C ./DSP_Lib/GCC -f arm_cortexM_math_m7bsfsp.mk clean
 	make -s -C ./DSP_Lib/GCC -f arm_cortexM_math_m7lsfdp.mk clean
 	make -s -C ./DSP_Lib/GCC -f arm_cortexM_math_m7bsfdp.mk clean
-	rm -rf ./CMSIS
+	$(RM) $(RMFLAGS) $(CMSISPKG)
     
 info :
 	make -s -C ./DSP_Lib/GCC -f arm_cortexM_math_m4lsf.mk info
